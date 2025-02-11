1de7c9cffb4cb33cf0f930ea2635da3b137903d9
marketbytes-official <info@marketbytes.in>
2025-02-11 15:31:00 +0530

    deleted all the files

--- social_media/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SocialMedia
-
-class SocialMediaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SocialMedia
-        fields = '__all__'
